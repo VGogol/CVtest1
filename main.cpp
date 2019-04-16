9682be28867c8b9d8a4c8c18adc710c76340217b
VGogol <luowenguo123@163.com>
2019-04-16 15:21:31 +0800

    change to erode

--- main.cpp
+++ main.cpp
@@ -5,5 +5,12 @@ int main()
 {
 	Mat  image = imread("3.jpg");
 	imshow("显示图像", image);
-	waitKey(6000);
+	//进行腐蚀操作
+	Mat  element = getStructuringElement(MORPH_RECT, Size(15, 15));
+	Mat  dstimag;
+	erode(image, dstimag, element);
+	//显示效果图
+	imshow("腐蚀操作效果图", dstimag);
+	waitKey(0);
+	return 0;
 }
