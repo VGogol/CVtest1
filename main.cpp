131656556cd8d18d848b447668e6b00f6c431fbd
VGogol <luowenguo123@163.com>
2019-04-24 20:53:54 +0800

    generate a image and write it in project path

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<opencv2\opencv.hpp>
 using namespace  cv;
+using namespace  std;
 
 //图像模糊
 void GOBlur()
@@ -75,10 +76,52 @@ void  GOReadVideo()
 	}
 }
 
+//创建带alpha通道的mat
+void  CreateAlphaMat(Mat  &mat)
+{
+	for (int i = 0; i < mat.rows; i++)
+	{
+		for (int j = 0; j < mat.cols; j++)
+		{
+			Vec4b&rgba = mat.at<	Vec4b>(i, j);
+			rgba[0] = UCHAR_MAX;
+			rgba[1] = saturate_cast<uchar>((float(mat.cols - j)) / ((float)mat.cols)*UCHAR_MAX);
+			rgba[2] = saturate_cast<uchar>((float(mat.rows - i)) / ((float)mat.rows)*UCHAR_MAX);
+			rgba[3] = saturate_cast < uchar > (0.5*(rgba[1] + rgba[2]));
+		}
+	}
+}
+
+//将png图片写入到当前工程目录下
+void  WritePNG()
+{
+	Mat   mat(480, 640, CV_8UC4);
+	CreateAlphaMat(mat);
+	vector<int>compression_params;
+	compression_params.push_back(IMWRITE_PNG_COMPRESSION);
+	compression_params.push_back(9);
+
+	try
+	{
+		imwrite("透明alpha值图.png", mat, compression_params);
+		imshow("生成的PNG图", mat);
+		fprintf(stdout, "PNG图片文件的alpha数据保存完毕~\n可以在工程目录下查看由imwrite函数生成的图片\n");
+		waitKey(0);
+	}
+	catch (runtime_error &ex)
+	{
+		fprintf(stderr, "图像转换成PNG格式发生错误：%s\n", ex.what());
+
+	}
+}
+
 void main()
 {
-	printf("当前OpenCV版本为 OpenCV %s", CV_VERSION);
-	system("pause");
+	//显示版本
+	/*printf("当前OpenCV版本为 OpenCV %s", CV_VERSION);
+	system("pause");*/
+	WritePNG();
+	
 	//GOCanny();
 	//GOReadVideo();
 	//getchar();
